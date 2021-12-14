438245b95601dd59514292f68bf0399c86eae216
VitaliyApanasevich <apanasevich-90@mail.ru>
2021-12-15 01:34:23 +0300

    Add senings emails

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,4 +1,5 @@
 const productsContainer = document.querySelector("#prod_container");
+const ordersContainer = document.getElementsByTagName("body");
 
 function loadProducts() {
     fetch("/api/v1/flight/all")
@@ -12,6 +13,88 @@ function loadProducts() {
                     productsContainer.appendChild(productsComponent(product));
                 })
             }
+        });
+}
+
+function loadUsers() {
+    const token = window.localStorage.getItem("JWT");
+    const id = window.localStorage.getItem("accountId");
+    const result2 = document.querySelector("#userInfo")
+    fetch(`/api/v1/user/info?accountId=${id}`, {
+        headers:{"Authorization":`Bearer ${token}`}}
+    )
+        .then(response => response.json())
+        .then(user => {
+            console.log(user)
+                    // result2.innerHTML += "<div style='display: flex; flex-direction: column'>"
+                    //     result2.innerHTML += "<div><h2 style='color: #007bff'>Email:</h2><h4>"+user.email+"</h4></div>";
+                    //     result2.innerHTML += "<div><h2 style='color: #007bff'>Login:<h2><h4>"+user.login+"</h4></div>";
+                    //     // result2.innerHTML += "<div><h2>Password:</h2><h4>"+user.password+"</h4></div>";
+                    // result2.innerHTML += "</div>"
+
+            result2.appendChild((() => {
+                const div = document.createElement('div');
+                div.style = "display: flex; flex-direction: column";
+                div.appendChild((() => {
+                    const p = document.createElement('p')
+                    p.innerHTML = `Email: ${user.email}`;
+
+                    return p;
+                })());
+                div.appendChild((() => {
+                    const p = document.createElement('p')
+                    p.innerHTML = `Login: ${user.login}`;
+
+                    return p;
+                })());
+                div.appendChild((() => {
+                    const p = document.createElement('h2')
+                    p.innerHTML = `ORDERS:`;
+
+                    return p;
+                })());
+                return div;
+            })());
+
+            user.orders.forEach(order => {
+                result2.appendChild((() => {
+                    const div = document.createElement('div');
+                    div.style = "display: flex; flex-direction: column";
+                    fetch(`/api/v1/flight/byid?id=${order.flightId}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data)
+
+                            div.appendChild((() => {
+                                const p = document.createElement('p')
+                                p.innerHTML = `Name:${data.name}`;
+                                return p;
+                            })());
+                            div.appendChild((() => {
+                                const p = document.createElement('p')
+                                p.innerHTML = `DateFrom:${data.date_from}`;
+                                return p;
+                            })());
+                            div.appendChild((() => {
+                                const p = document.createElement('p')
+                                p.innerHTML = `DateTo:${data.date_to}`;
+                                return p;
+                            })());
+                            div.appendChild((() => {
+                                const p = document.createElement('p')
+                                p.innerHTML = `Price:${data.price}`;
+                                return p;
+                            })());
+                            div.appendChild((() => {
+                                const p = document.createElement('p')
+                                p.innerHTML = `----------------------------------------------------`;
+                                return p;
+                            })());
+                        });
+
+                    return div;
+                })());
+            })
         });
 }
 
@@ -36,13 +119,13 @@ function productsComponent(product) {
     p1.innerHTML = "From";
     const p2 = document.createElement("p");
     p2.classList.add("card-text");
-    p2.innerHTML = product.date_from;
+    p2.innerHTML = new Date(product.date_from).toDateString() + ' ' + new Date(product.date_from).toTimeString();
     const p3 = document.createElement("p");
     p3.classList.add("card-text");
     p3.innerHTML = "To";
     const p4 = document.createElement("p");
     p4.classList.add("card-text");
-    p4.innerHTML = product.date_to;
+    p4.innerHTML =  new Date(product.date_to).toDateString() + ' ' + new Date(product.date_to).toTimeString();;
     const p5 = document.createElement("p")
     p5.classList.add("card-text");
     p5.innerHTML = product.price+'$';
@@ -74,6 +157,15 @@ function productsComponent(product) {
             } else {
                 alert("Order was created and bills will be send on the email")
             }
+        })
+        fetch("/api/v1/mail/send", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token},
+            body: JSON.stringify({
+                count: 1,
+                accountId,
+                productId: product.id
+            })
         })
     }
 
